4e5871d9083206ac2af4a3241b0c9dc3b73259e8
failfmi <oscurocalma@gmail.com>
2022-11-24 18:36:17 +0200

    feat(option): decode functionality

--- option.go
+++ option.go
@@ -2,26 +2,17 @@ package goscale
 
 import "bytes"
 
-/*
-	https://spec.polkadot.network/#defn-option-type)
-
-	SCALE Option Type ...
-
-	https://spec.polkadot.network/#defn-result-type)
-
-	SCALE Result Type ...
-*/
-
-// Option is a structure that can store a boolean or a missing value.
-// Encoding rules are slightly different from other "Option" fields.
+// Option is a varying data structure that can store an Encodable Value.
+// HasValue indicates if Value is available.
+// Ref: https://spec.polkadot.network/#defn-option-type)
 type Option[T Encodable] struct {
-	hasValue bool
+	HasValue bool
 	Value    Encodable
 }
 
 func (o Option[T]) Encode(buffer *bytes.Buffer) {
 	encoder := Encoder{Writer: buffer}
-	if !o.hasValue {
+	if !o.HasValue {
 		encoder.EncodeByte(0)
 	} else {
 		encoder.EncodeByte(1)
@@ -29,17 +20,39 @@ func (o Option[T]) Encode(buffer *bytes.Buffer) {
 	}
 }
 
-func DecodeOption[T Encodable](dec interface{}, buffer *bytes.Buffer) Option[T] {
+func DecodeOption[T Encodable](dec Encodable, buffer *bytes.Buffer) Option[T] {
 	b := DecodeBool(buffer)
 
 	option := Option[T]{
-		hasValue: b == true,
+		HasValue: b == true,
 	}
 
 	if b {
 		switch dec.(type) {
 		case Bool:
 			option.Value = DecodeBool(buffer)
+		case U8:
+			option.Value = DecodeU8(buffer)
+		case I8:
+			option.Value = DecodeI8(buffer)
+		case U16:
+			option.Value = DecodeU16(buffer)
+		case I16:
+			option.Value = DecodeI16(buffer)
+		case U32:
+			option.Value = DecodeU32(buffer)
+		case I32:
+			option.Value = DecodeI32(buffer)
+		case U64:
+			option.Value = DecodeU64(buffer)
+		case I64:
+			option.Value = DecodeI64(buffer)
+		case Compact:
+			option.Value = DecodeCompact(buffer)
+		case Sequence[U8]:
+			option.Value = DecodeSequenceU8(buffer)
+		default:
+			panic("type not found")
 		}
 	}
 
