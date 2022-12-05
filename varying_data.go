554ee05b08447d311f8f6c4c4c917d49804476f2
failfmi <oscurocalma@gmail.com>
2022-12-05 12:42:58 +0200

    feat(varying-data): add decode; switch underlying structure

--- varying_data.go
+++ varying_data.go
@@ -4,42 +4,42 @@ import (
 	"bytes"
 )
 
-type VaryingData struct {
-	Values map[U8]Encodable
-}
+type VaryingData []Encodable
 
 func NewVaryingData(values ...Encodable) VaryingData {
 	if len(values) > 255 {
-		panic("invalid length")
-	}
-	i := 0
-	result := map[U8]Encodable{}
-
-	for _, v := range values {
-		result[U8(i)] = v
-		i++
-	}
-
-	return VaryingData{
-		Values: result,
-	}
-}
-
-func (vd *VaryingData) Add(values ...Encodable) {
-	l := len(vd.Values)
-	if l+len(values) > 255 {
 		panic("exceeds uint8 length")
 	}
 
+	var result []Encodable
 	for _, v := range values {
-		vd.Values[U8(l)] = v
-		l++
+		result = append(result, v)
+	}
+
+	return result
+}
+
+func (vd VaryingData) Encode(buffer *bytes.Buffer) {
+	for k, v := range vd {
+		U8(k).Encode(buffer)
+		v.Encode(buffer)
 	}
 }
 
-func (vd *VaryingData) Encode(buffer *bytes.Buffer) {
-	for k, v := range vd.Values {
-		k.Encode(buffer)
-		v.Encode(buffer)
+func DecodeVaryingData(values []Encodable, buffer *bytes.Buffer) VaryingData {
+	vLen := len(values)
+	if vLen > 255 {
+		panic("exceeds uint8 length")
 	}
+
+	result := make([]Encodable, vLen)
+	for i := 0; i < len(values); i++ {
+
+		key := DecodeU8(buffer)
+		value := decodeByType(values[key], buffer)
+
+		result[key] = value
+	}
+
+	return result
 }
--- varying_data_test.go
+++ varying_data_test.go
@@ -2,16 +2,18 @@ package goscale
 
 import (
 	"bytes"
+	"math"
 	"testing"
 )
 
-func Test_EncodeVaryingData(t *testing.T) {
+func Test_VaryingData_Encode(t *testing.T) {
 	var examples = []struct {
 		label  string
 		input  VaryingData
 		expect []byte
 	}{
-		{label: "Encode VaryingData(42, true)", input: NewVaryingData(U8(42), Bool(true)), expect: []byte{0x0, 0x2a, 0x1, 0x1}},
+		{label: "Encode VaryingData(U8, Bool)", input: NewVaryingData(U8(42), Bool(true)), expect: []byte{0x0, 0x2a, 0x1, 0x1}},
+		{label: "Encode VaryingData(U128, Empty)", input: NewVaryingData(U128{math.MaxUint64, math.MaxUint64}, Empty{}), expect: []byte{0x0, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x1}},
 	}
 
 	for _, e := range examples {
@@ -38,27 +40,45 @@ func Test_NewVaryingData_InvalidLength(t *testing.T) {
 	})
 }
 
-func Test_VaryingData_Add_InvalidLength(t *testing.T) {
+func Test_VaryingData_Decode(t *testing.T) {
+	var examples = []struct {
+		label  string
+		input  []byte
+		order  []Encodable
+		expect VaryingData
+	}{
+		{
+			label:  "Decode VaryingData(U8, Bool)",
+			input:  []byte{0x0, 0x2a, 0x1, 0x1},
+			order:  []Encodable{U8(1), Bool(false)},
+			expect: NewVaryingData(U8(42), Bool(true))},
+		{
+			label:  "DecodeVaryingData(U128, Empty)",
+			input:  []byte{0x0, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x1},
+			order:  []Encodable{U128{0, 0}, Empty{}},
+			expect: NewVaryingData(U128{math.MaxUint64, math.MaxUint64}, Empty{}),
+		},
+	}
+
+	for _, e := range examples {
+		// given:
+		buffer := &bytes.Buffer{}
+		buffer.Write(e.input)
+
+		// when:
+		result := DecodeVaryingData(e.order, buffer)
+
+		// then:
+		assertEqual(t, result, e.expect)
+	}
+}
+
+func Test_VaryingData_Panic(t *testing.T) {
 	// given:
 	values := make([]Encodable, 256)
-	varyingData := NewVaryingData()
 
 	// then:
 	assertPanic(t, func() {
-		varyingData.Add(values...)
+		DecodeVaryingData(values, &bytes.Buffer{})
 	})
-}
-
-func Test_VaryingData_Add(t *testing.T) {
-	// given:
-	values := []Encodable{Bool(true), U8(42)}
-	varyingData := NewVaryingData()
-
-	// when:
-	varyingData.Add(values...)
-
-	// then:
-	for i, v := range values {
-		assertEqual(t, varyingData.Values[U8(i)], v)
-	}
 }
