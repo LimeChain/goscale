9fec230b00e8d2b0614a1f3919a4a9ee055fff11
Rado M <radkomih@gmail.com>
2022-12-09 16:20:40 +0200

    Tuple refactor

--- tuple.go
+++ tuple.go
@@ -11,143 +11,201 @@ import (
 	SCALE Tuple type translates to Go's struct type.
 */
 
-type Tuple interface{}
+type Tuple[T any] struct {
+	Data T
+}
 
-func EncodeTuple(value Tuple, buffer *bytes.Buffer) {
-	t := reflect.TypeOf(value)
+func (t Tuple[Encodable]) Encode(buffer *bytes.Buffer) {
+	tVal := reflect.ValueOf(t.Data)
+	tTyp := reflect.TypeOf(t.Data)
 
-	if t.Kind() != reflect.Struct {
-		panic("not a Tuple type")
+	if tTyp.Kind() != reflect.Struct {
+		panic("not a SCALE Tuple type")
 	}
 
-	v := reflect.ValueOf(value)
+	// for i, field := range reflect.VisibleFields(tt) {}
+	for i := 0; i < tVal.NumField(); i++ {
+		fieldT := tVal.Field(i)
+		fieldV := tTyp.Field(i)
 
-	for i := 0; i < v.NumField(); i++ {
-		field := v.Field(i)
+		if fieldV.IsExported() {
+			println("field:", i, fieldV.Name)
 
-		if t.Field(i).IsExported() {
-			switch field.Kind() {
+			switch fieldT.Kind() {
 			case reflect.Bool:
-				(field.Interface().(Bool)).Encode(buffer)
-			case reflect.Int:
-				(field.Interface().(Compact)).Encode(buffer)
+				(fieldT.Interface().(Bool)).Encode(buffer)
 			case reflect.Uint:
-				(field.Interface().(Compact)).Encode(buffer)
-			case reflect.Int8:
-				(field.Interface().(I8)).Encode(buffer)
+				(fieldT.Interface().(Compact)).Encode(buffer)
+			case reflect.Int:
+				(fieldT.Interface().(Compact)).Encode(buffer)
 			case reflect.Uint8:
-				(field.Interface().(U8)).Encode(buffer)
-			case reflect.Int16:
-				(field.Interface().(I16)).Encode(buffer)
+				(fieldT.Interface().(U8)).Encode(buffer)
+			case reflect.Int8:
+				(fieldT.Interface().(I8)).Encode(buffer)
 			case reflect.Uint16:
-				(field.Interface().(U16)).Encode(buffer)
-			case reflect.Int32:
-				(field.Interface().(I32)).Encode(buffer)
+				(fieldT.Interface().(U16)).Encode(buffer)
+			case reflect.Int16:
+				(fieldT.Interface().(I16)).Encode(buffer)
 			case reflect.Uint32:
-				(field.Interface().(U32)).Encode(buffer)
-			case reflect.Int64:
-				(field.Interface().(I64)).Encode(buffer)
+				(fieldT.Interface().(U32)).Encode(buffer)
+			case reflect.Int32:
+				(fieldT.Interface().(I32)).Encode(buffer)
 			case reflect.Uint64:
-				(field.Interface().(U64)).Encode(buffer)
+				(fieldT.Interface().(U64)).Encode(buffer)
+			case reflect.Int64:
+				(fieldT.Interface().(I64)).Encode(buffer)
 			case reflect.String:
-				(field.Interface().(Str)).Encode(buffer)
+				(fieldT.Interface().(Str)).Encode(buffer)
+
+			// TODO: handle Sequence of { Sequence | Fixed Sequence | Dictionary | Tuple }
 			case reflect.Slice:
-				// TODO: handle Sequence of { Sequence | Fixed Sequence | Dictionary | Tuple }
-				elemType := reflect.SliceOf(field.Type())
+				elemType := reflect.SliceOf(fieldT.Type())
 
 				if elemType == reflect.TypeOf([][]Bool{}) {
-					elemValues, ok := field.Interface().([]Bool)
+					elemValues, ok := fieldT.Interface().([]Bool)
 					if !ok {
 						panic("unable to convert to Sequence[Bool]")
 					}
-					Sequence[Bool]{Values: elemValues}.Encode(buffer)
+					Sequence[Bool](elemValues).Encode(buffer)
 
-				} else if elemType == reflect.TypeOf([][]Compact{}) {
-					elemValues, ok := field.Interface().([]Compact)
-					if !ok {
-						panic("unable to convert to Sequence[Compact]")
-					}
-					Sequence[Compact]{Values: elemValues}.Encode(buffer)
+					// } else if elemType == reflect.TypeOf([][]Compact{}) {
+					// 	elemValues, ok := fieldT.Interface().([]Compact)
+					// 	if !ok {
+					// 		panic("unable to convert to Sequence[Compact]")
+					// 	}
+					// 	Sequence[Compact](elemValues).Encode(buffer)
 
-				} else if elemType == reflect.TypeOf([][]U8{}) {
-					elemValues, ok := field.Interface().([]U8)
-					if !ok {
-						panic("unable to convert to Sequence[U8]")
-					}
-					Sequence[U8]{Values: elemValues}.Encode(buffer)
+					// } else if elemType == reflect.TypeOf([][]U8{}) {
+					// 	elemValues, ok := fieldT.Interface().([]U8)
+					// 	if !ok {
+					// 		panic("unable to convert to Sequence[U8]")
+					// 	}
+					// 	Sequence[U8](elemValues).Encode(buffer)
 
-				} else if elemType == reflect.TypeOf([][]I8{}) {
-					elemValues, ok := field.Interface().([]I8)
-					if !ok {
-						panic("unable to convert to Sequence[I8]")
-					}
-					Sequence[I8]{Values: elemValues}.Encode(buffer)
+					// } else if elemType == reflect.TypeOf([][]I8{}) {
+					// 	elemValues, ok := fieldT.Interface().([]I8)
+					// 	if !ok {
+					// 		panic("unable to convert to Sequence[I8]")
+					// 	}
+					// 	Sequence[I8](elemValues).Encode(buffer)
 
-				} else if elemType == reflect.TypeOf([][]U16{}) {
-					elemValues, ok := field.Interface().([]U16)
-					if !ok {
-						panic("unable to convert to Sequence[U16]")
-					}
-					Sequence[U16]{Values: elemValues}.Encode(buffer)
+					// } else if elemType == reflect.TypeOf([][]U16{}) {
+					// 	elemValues, ok := fieldT.Interface().([]U16)
+					// 	if !ok {
+					// 		panic("unable to convert to Sequence[U16]")
+					// 	}
+					// 	Sequence[U16](elemValues).Encode(buffer)
 
-				} else if elemType == reflect.TypeOf([][]I16{}) {
-					elemValues, ok := field.Interface().([]I16)
-					if !ok {
-						panic("unable to convert to Sequence[I16]")
-					}
-					Sequence[I16]{Values: elemValues}.Encode(buffer)
+					// } else if elemType == reflect.TypeOf([][]I16{}) {
+					// 	elemValues, ok := fieldT.Interface().([]I16)
+					// 	if !ok {
+					// 		panic("unable to convert to Sequence[I16]")
+					// 	}
+					// 	Sequence[I16](elemValues).Encode(buffer)
 
-				} else if elemType == reflect.TypeOf([][]U32{}) {
-					elemValues, ok := field.Interface().([]U32)
-					if !ok {
-						panic("unable to convert to Sequence[U32]")
-					}
-					Sequence[U32]{Values: elemValues}.Encode(buffer)
+					// } else if elemType == reflect.TypeOf([][]U32{}) {
+					// 	elemValues, ok := fieldT.Interface().([]U32)
+					// 	if !ok {
+					// 		panic("unable to convert to Sequence[U32]")
+					// 	}
+					// 	Sequence[U32](elemValues).Encode(buffer)
 
-				} else if elemType == reflect.TypeOf([][]I32{}) {
-					elemValues, ok := field.Interface().([]I32)
-					if !ok {
-						panic("unable to convert to Sequence[I32]")
-					}
-					Sequence[I32]{Values: elemValues}.Encode(buffer)
+					// } else if elemType == reflect.TypeOf([][]I32{}) {
+					// 	elemValues, ok := fieldT.Interface().([]I32)
+					// 	if !ok {
+					// 		panic("unable to convert to Sequence[I32]")
+					// 	}
+					// 	Sequence[I32](elemValues).Encode(buffer)
 
-				} else if elemType == reflect.TypeOf([][]U64{}) {
-					elemValues, ok := field.Interface().([]U64)
-					if !ok {
-						panic("unable to convert to Sequence[U64]")
-					}
-					Sequence[U64]{Values: elemValues}.Encode(buffer)
+					// } else if elemType == reflect.TypeOf([][]U64{}) {
+					// 	elemValues, ok := fieldT.Interface().([]U64)
+					// 	if !ok {
+					// 		panic("unable to convert to Sequence[U64]")
+					// 	}
+					// 	Sequence[U64](elemValues).Encode(buffer)
 
-				} else if elemType == reflect.TypeOf([][]I64{}) {
-					elemValues, ok := field.Interface().([]I64)
-					if !ok {
-						panic("unable to convert to Sequence[I64]")
-					}
-					Sequence[I64]{Values: elemValues}.Encode(buffer)
+					// } else if elemType == reflect.TypeOf([][]I64{}) {
+					// 	elemValues, ok := fieldT.Interface().([]I64)
+					// 	if !ok {
+					// 		panic("unable to convert to Sequence[I64]")
+					// 	}
+					// 	Sequence[I64](elemValues).Encode(buffer)
 
+					// handle Sequence|FixedSequence types below
+				} else if elemType == reflect.TypeOf([]Sequence[Bool]{}) {
+					tryConvert[Sequence[Bool]](fieldT).Encode(buffer)
+				} else if elemType == reflect.TypeOf([]Sequence[U8]{}) {
+					tryConvert[Sequence[U8]](fieldT).Encode(buffer)
+				} else if elemType == reflect.TypeOf([]Sequence[I8]{}) {
+					tryConvert[Sequence[I8]](fieldT).Encode(buffer)
+				} else if elemType == reflect.TypeOf([]Sequence[U16]{}) {
+					tryConvert[Sequence[U16]](fieldT).Encode(buffer)
+				} else if elemType == reflect.TypeOf([]Sequence[I16]{}) {
+					tryConvert[Sequence[I16]](fieldT).Encode(buffer)
+				} else if elemType == reflect.TypeOf([]Sequence[U32]{}) {
+					tryConvert[Sequence[U32]](fieldT).Encode(buffer)
+				} else if elemType == reflect.TypeOf([]Sequence[I32]{}) {
+					tryConvert[Sequence[I32]](fieldT).Encode(buffer)
+				} else if elemType == reflect.TypeOf([]Sequence[U64]{}) {
+					tryConvert[Sequence[U64]](fieldT).Encode(buffer)
+				} else if elemType == reflect.TypeOf([]Sequence[I64]{}) {
+					tryConvert[Sequence[I64]](fieldT).Encode(buffer)
 				} else {
-					// panic("Tuple type encoding is not implemented")
-					EncodeTuple(field, buffer)
+					panic("case Slice: unknown type: " + fieldT.Type().Name())
 				}
+			// some SCALE defined types are represented as an array
 			case reflect.Array:
-				panic("encoding of type Fixed Sequence is not implemented")
+				switch fieldT.Type().Name() {
+				case "U128":
+					fieldT.Interface().(U128).Encode(buffer)
+				case "I128":
+					fieldT.Interface().(I128).Encode(buffer)
+				default:
+					panic("case Array: " + fieldT.Type().Name())
+				}
+			// some SCALE defined types are represented as struct
 			case reflect.Struct:
-				EncodeTuple(field.Interface(), buffer)
+				switch fieldT.Type().Name() {
+				case "Empty":
+					fieldT.Interface().(Empty).Encode(buffer)
+				case "CompactU128":
+					fieldT.Interface().(CompactU128).Encode(buffer)
+				default:
+					panic("case Struct: " + fieldT.Type().Name())
+				}
 			case reflect.Map:
-				panic("encoding of type Dictionary is not implemented")
-			// case reflect.Float32:
-			// case reflect.Float64:
-			// case reflect.Complex64:
-			// case reflect.Complex128:
-			// case reflect.Uintptr:
-			// case reflect.UnsafePointer:
-			// case reflect.Pointer:
-			// case reflect.Chan:
-			// case reflect.Func:
-			// case reflect.Interface:
+				panic("encoding of SCALE Dictionary field is not implemented")
+			case reflect.Float32:
+				panic("encoding of Float32 field is not implemented")
+			case reflect.Float64:
+				panic("encoding of Float64 field is not implemented")
+			case reflect.Complex64:
+				panic("encoding of Complex64 field is not implemented")
+			case reflect.Complex128:
+				panic("encoding of Complex128 field is not implemented")
+			case reflect.Uintptr:
+				panic("encoding of Uintptr field is not implemented")
+			case reflect.UnsafePointer:
+				panic("encoding of UnsafePointer field is not implemented")
+			case reflect.Pointer:
+				panic("encoding of Pointer field is not implemented")
+			case reflect.Chan:
+				panic("encoding of Chan field is not implemented")
+			case reflect.Func:
+				panic("encoding of Func field is not implemented")
+			case reflect.Interface:
+				panic("encoding of Interface field is not implemented")
 			default:
 				panic("unreachable case")
 			}
 		}
 	}
+}
+
+func tryConvert[T Encodable](v reflect.Value) Encodable {
+	value, ok := v.Interface().(T)
+	if !ok {
+		panic("unable to convert to []" + reflect.TypeOf(*new(T)).Name())
+	}
+	return value
 }
--- tuple_test.go
+++ tuple_test.go
@@ -5,74 +5,112 @@ import (
 	"testing"
 )
 
-type ApiItem struct {
-	Name    FixedSequence[U8]
-	Version U32
+type NestedStruct struct {
+	AA Bool
+	BB Empty
+	CC Str
 }
 
-func (api ApiItem) Encode(buffer *bytes.Buffer) {
-	EncodeTuple(api, buffer)
-}
+type ExampleStruct struct {
+	A  Bool
+	B  U8
+	C  U16
+	D  U32
+	E  U64
+	F  U128
+	G  I8
+	H  I16
+	I  I32
+	J  I64
+	K  I128
+	L0 Compact
+	L  CompactU128
+	M0 Str
+	M  Sequence[I32]
+	// N  FixedSequence[U8]   // panic: encoding of SCALE FixedSequence[goscale.U8] field is not implemented
+	// O  Dictionary[Str, U8] // panic: encoding of type Dictionary is not implemented
+	P Empty
 
-type VersionData struct {
-	SpecName           Str
-	ImplName           Str
-	AuthoringVersion   U32
-	SpecVersion        U32
-	ImplVersion        U32
-	Apis               Sequence[ApiItem]
-	TransactionVersion U32
-	StateVersion       U8
+	// Q Option[U8]
+	// R Result[U8]
+	// S VaryingData
+	// T NestedStruct // panic: encoding of type Nested Tuple is not implemented
+	// U Sequence[Tuple[NestedStruct]]
 }
 
 func Test_EncodeTuple(t *testing.T) {
-	input := VersionData{
-		SpecName:         Str("polkadot"),
-		ImplName:         Str("parity-polkadot"),
-		AuthoringVersion: U32(1),
-		SpecVersion:      U32(2),
-		ImplVersion:      U32(3),
-		Apis: Sequence[ApiItem]{
-			Values: []ApiItem{
-				{Name: FixedSequence[U8]{Values: []U8{6, 7, 8}}, Version: U32(9)},
-			},
+	buffer := &bytes.Buffer{}
+	Tuple[ExampleStruct]{
+		Data: ExampleStruct{
+			A:  true,
+			B:  1,
+			C:  2,
+			D:  3,
+			E:  4,
+			F:  U128{},
+			G:  -1,
+			H:  -2,
+			I:  -3,
+			J:  -4,
+			K:  I128{},
+			L0: Compact(1),
+			L:  CompactU128{},
+			M:  Sequence[I32]{1, 2, 3},
+			// N: FixedSequence[U8]{3},
+			// O: Dictionary[Str, U8]{},
+			P: Empty{},
+
+			// Q: Option[U8]{},
+			// R: Result[U8]{},
+			// S: VaryingData{},
+			// T: NestedStruct{},
+			// U: Sequence[Tuple[NestedStruct]]{},
 		},
-		TransactionVersion: U32(4),
-		StateVersion:       U8(5),
-	}
+	}.Encode(buffer)
+	t.Logf("\n %#x \n", buffer)
 
-	var testExamples = []struct {
-		label       string
-		input       VersionData
-		expectation []byte
-	}{
-		{
-			label: "VersionData{}",
-			input: input,
-			expectation: []byte{
-				0x0c, 0x61, 0x62, 0x63,
-				0x0c, 0x78, 0x79, 0x7a,
-				0x01, 0x00, 0x00, 0x00,
-				0x02, 0x00, 0x00, 0x00,
-				0x03, 0x00, 0x00, 0x00,
-				0x04, 0x00, 0x00, 0x00,
-				0x05,
+	// input := VersionData{
+	// 	SpecName:         Str("abc"),
+	// 	ImplName:         Str("xyz"),
+	// 	AuthoringVersion: U32(1),
+	// 	SpecVersion:      U32(2),
+	// 	ImplVersion:      U32(3),
+	// 	// Apis: Sequence[ApiItem]{
+	// 	// 	Values: []ApiItem{
+	// 	// 		{Name: FixedSequence[U8]{Values: []U8{6, 7, 8}}, Version: U32(9)},
+	// 	// 	},
+	// 	// },
+	// 	TransactionVersion: U32(4),
+	// 	StateVersion:       U8(5),
+	// }
 
-				// 20 70 6f 6c 6b 61 64 6f 74 3c 70 61 72 69 74 79 2d 70 6f 6c 6b 61 64 6f 74
-				// 01 00 00 00
-				// 02 00 00 00
-				// 030000000400000005
-			},
-		},
-	}
+	// var testExamples = []struct {
+	// 	label       string
+	// 	input       VersionData
+	// 	expectation []byte
+	// }{
+	// 	{
+	// 		label: "VersionData{}",
+	// 		input: input,
+	// 		expectation: []byte{
+	// 			0x0c, 0x61, 0x62, 0x63,
+	// 			0x0c, 0x78, 0x79, 0x7a,
+	// 			0x01, 0x00, 0x00, 0x00,
+	// 			0x02, 0x00, 0x00, 0x00,
+	// 			0x03, 0x00, 0x00, 0x00,
+	// 			0x04, 0x00, 0x00, 0x00,
+	// 			0x05,
+	// 		},
+	// 	},
+	// }
 
-	for _, testExample := range testExamples {
-		t.Run(testExample.label, func(t *testing.T) {
-			buffer := &bytes.Buffer{}
+	// for _, testExample := range testExamples {
+	// 	t.Run(testExample.label, func(t *testing.T) {
+	// 		buffer := &bytes.Buffer{}
 
-			EncodeTuple(testExample.input, buffer)
+	// 		EncodeTuple(testExample.input, buffer)
 
-			assertEqual(t, buffer.Bytes(), testExample.expectation)
-		})
-	}
+	// 		assertEqual(t, buffer.Bytes(), testExample.expectation)
+	// 	})
+	// }
 }
